2d21f8f97dafb6f37184a19850d48cc96706e24a
Sergey Uvarov <10648158+Sergibon@users.noreply.github.com>
2026-02-21 17:05:24 +0300

    Remove YouTube match and exclude rules
    Removed match and exclude rules for YouTube.

--- AdGuard/scripts.js
+++ AdGuard/scripts.js
@@ -2,14 +2,6 @@
 // @name         [SU] Scripts
 // @version      0.0
 // @encoding     utf-8
-// @match        *://*.youtube.com/*
-// @exclude      *://music.youtube.com/*
-// @exclude      *://*.music.youtube.com/*
-// @compatible   chrome
-// @compatible   firefox
-// @compatible   opera
-// @compatible   safari
-// @compatible   edge
 // @downloadURL  https://sergibon.github.io/AdGuard/scripts.js
 // @updateURL    https://sergibon.github.io/AdGuard/scripts.js
 // ==/UserScript==
